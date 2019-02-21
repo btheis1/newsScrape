90b60b32b8a144749109c38afb23230ae55dccf7
C Howe <brook.theis@gmail.com>
2019-02-20 18:24:03 -0700

    scrape functioning

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+
 var db = require("./models");
 
 var PORT = 3000;
@@ -11,7 +12,7 @@ var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -23,24 +24,28 @@ app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
 });
 
+app.get("/", function(req, res) {
+    res.render("index");
+})
 //ROUTES
 app.get("/scrape", function(req, res) {
-    axios.get("https://www.bbc.com/news").then(function(response) {
+    console.log("scrape hit")
+    axios.get("https://www.nytimes.com/section/world").then(function(response) {
         var $ = cheerio.load(response.data);
 
-        $("story-body h1").each(function(i, element) {
+        $("#stream-panel li").each(function(i, element) {
             var result = {};
-
+            
             result.headline = $(this)
-                .children("a")
+                .find("h2")
                 .text();
             result.summary = $(this)
-                .children("a")
+                .find("p")
                 .text();
             result.URL = $(this)
-                .children("a")
+                .find("a")
                 .attr("href");
-            
+            console.log(result);
             db.Article.create(result)
                 .then(function(dbArticle) {
                     console.log(dbArticle);
@@ -49,8 +54,41 @@ app.get("/scrape", function(req, res) {
                     console.log(err);
                 });
         });
-
+        
         res.send("Scrape Complete");
     });
+});
 
-})
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle)
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id}, { new: true})
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        }) 
+        .catch(function(err) {
+            res.json(err);
+        });
+});
